e85693b21718afb27bebfe4de9cfd630a253b9ac
shivamsri1 <shivam.srivastava@novostack.com>
2025-12-15 18:31:50 +0530

    Fix Playwright CI setup and ignore cache files

--- conftest.py
+++ conftest.py
@@ -1,12 +1,15 @@
+import os
 import pytest
 from playwright.sync_api import sync_playwright
 
 @pytest.fixture
 def page():
+    is_ci = os.getenv("CI", "false").lower() == "true"
+
     with sync_playwright() as p:
         browser = p.chromium.launch(
-            headless=False,   # 👈 SHOW browser
-            slow_mo=500       # 👈 slow actions (optional)
+            headless=is_ci,   # 👈 CI = headless, Local = headed
+            slow_mo=500 if not is_ci else 0
         )
         context = browser.new_context()
         page = context.new_page()
